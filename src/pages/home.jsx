3ff707a99c76f58f7841cc053a5a64c0bc041d9e
proxiner <mahdigamingcodez@gmail.com>
2024-07-09 23:41:45 +0330

    finished the home page with styling

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,36 +1,20 @@
 import { useState } from "react";
 import "./home.scss";
 
-function Home() {
-  const [count, setCount] = useState(0);
+import { Link } from "react-router-dom";
 
+function Home() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/assets/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img
-            src="/assets/react.svg"
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <section className="home">
+      <div className="welcome-message">
+        <h1> Welcome To Authenticate </h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          This project exist to achive the simple task of Registering a "user" &
+          Loging in the "user".
         </p>
+        <Link to={'/dashboard'}> Dashboard <div className="line"> </div> </Link>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </section>
   );
 }
 
