c4d26c794cb75ddd69fdb88a0814ee6f242a7570
proxiner <mahdigamingcodez@gmail.com>
2024-07-09 23:42:43 +0330

    installed and configured react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom/client";
 
 import Home from "./pages/home";
 
+import { BrowserRouter ,Routes , Route } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 );
